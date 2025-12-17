f3cb0f2b4f4ba830f0aaf14ea8407a660410804b
Soluciones Inteligentes <sinteligentesw@gmail.com>
2025-12-16 22:16:15 -0400

    subiendo build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://icap.lat',
         changeOrigin: true,
         secure: false
       }
